03e919550a63cdd251781f4e1c584b3fc57effa8
ABHILASH-XVII <100773547+ABHILASH-XVII@users.noreply.github.com>
2022-07-08 13:29:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,3 +57,39 @@ resource "azurerm_subnet_network_security_group_association""association2" {
   subnet_id                 = azurerm_subnet.subnet[1].id
   network_security_group_id = data.azurerm_network_security_group.rg.id
 }
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = var.ipconfig_name
+    subnet_id                     = azurerm_subnet.subnet[1].id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
